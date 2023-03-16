9564a2bda7bc98ae441af967c87aa7a6c1ac5553
aguusfer <90877454+aguusfer@users.noreply.github.com>
2023-03-16 18:48:55 -0300

    se implementa JuegoMesa

--- src/Objeto/JuegoMesa.cc
+++ src/Objeto/JuegoMesa.cc
@@ -1,1 +1,29 @@
-#include JuegoMesa.h 
+#include "JuegoMesa.h"
+
+JuegoMesa::JuegoMesa(string n, int a, Estado e, int edad, int c):Objeto(n,a,e) {
+    edadRecomendada=edad; //this->edadRecomendada = edad
+    cantJugadores=c; //this->cantJugadores = c;
+};
+
+JuegoMesa::JuegoMesa():Objeto(){}
+
+void JuegoMesa::setEdadRecomendada(int edad){
+    edadRecomendada=edad;
+};
+
+
+int JuegoMesa::setCantJugadores(int cant){
+    cantJugadores = cant;
+};
+
+int JuegoMesa::getEdadRecomendada(){
+    return edadRecomendada;
+};
+
+int JuegoMesa::getCantJugadores(){
+    return cantJugadores;
+};
+
+string JuegoMesa::toString(){
+    return 0;
+};
