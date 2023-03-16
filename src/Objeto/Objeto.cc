afa657e9a50cd888600b908cf7e1b97e5501c3ea
Federico Godin Noya <federico.godin@fing.edu.uy>
2023-03-16 20:33:56 -0300

    Update src/Objeto/Objeto.cc

--- src/Objeto/Objeto.cc
+++ src/Objeto/Objeto.cc
@@ -1,1 +1,33 @@
+#include "objeto.h"
 
+Objeto::Objeto(string n,int a,Estado e){
+    nombre=n;
+    añoComprado=a;
+    estado=e;
+}
+
+Objeto::Objeto(){}
+
+void Objeto::setNombre(string n){
+    nombre=n;
+}
+
+void Objeto::setAñoComprado(int a){
+    añoComprado=a;
+}
+
+void Objeto::setEstado(Estado e){
+    estado=e;
+}
+
+string Objeto::getNombre(){
+    return nombre;
+}
+
+int Objeto::getAñoComprado(){
+    return añoComprado;
+}
+
+Estado Objeto::getEstado(){
+    return estado;
+}
