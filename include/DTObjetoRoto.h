50da9e0a635629758f902b5422682d55331ce8c8
Federico Godin Noya <federico.godin@fing.edu.uy>
2023-03-17 18:50:57 -0300

    Update include/DTObjetoRoto.h

--- include/DTObjetoRoto.h
+++ include/DTObjetoRoto.h
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class DTObjetoRoto{
+    private:
+        string NombreObjeto;
+        bool prestado;
+        string NombreNinio;
+    public:
+        DTObjetoRoto();
+        DTObjetoRoto(string,bool,string);
+        string getNombreObjeto();
+        bool getPrestado();
+        string getNombreNinio();
+        ~DTObjetoRoto();
+};
 
