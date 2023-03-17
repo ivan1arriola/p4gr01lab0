1240bf87a4124f6e3be8981dee0e348603e74339
Agustina Isabel Fernandez Cabrera <agustina.fernandez@fing.edu.uy>
2023-03-17 16:32:09 -0300

    Cambios en main

---
+++ main.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+#include "JuegoMesa.h"
+#include "Objeto.h"
+  
+int main()
+{
+    //Creación de objetos JuegoMesa
+    //JuegoMesa Uno = new JuegoMesa("Juego Uno", 2022, Roto, 7, 10);
+    //JuegoMesa MozoCartas = new JuegoMesa("Mozo de Cartas", 2019, Nuevo, 7, 4);
+    //JuegoMesa Dados = new JuegoMesa("Dados", 2020, Roto, 2, 6);
+    return   0; 
+}
