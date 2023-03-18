a883e7ad0327e19e05b2c4fdae07a8e9675012fc
aguusfer <90877454+aguusfer@users.noreply.github.com>
2023-03-18 19:32:43 -0300

    Correción uso de Estado

--- main.cc
+++ main.cc
@@ -2,12 +2,20 @@
 using namespace std;
 #include "JuegoMesa.h"
 #include "Objeto.h"
+#include "Objeto.cc"
+#include "JuegoMesa.cc"
   
 int main()
 {
+    Estado estadoUno = Estado::Roto;
+    Estado estadoMozoCartas = Estado::Nuevo;
+    Estado estadoDados = Estado::Roto;
     //Creación de objetos JuegoMesa
+    JuegoMesa Uno = JuegoMesa("Juego Uno", 2022, estadoUno, 7, 10);
     //JuegoMesa Uno = new JuegoMesa("Juego Uno", 2022, Roto, 7, 10);
+    JuegoMesa MozoCartas = JuegoMesa("Mozo de Cartas", 2019, estadoMozoCartas, 7, 4);
     //JuegoMesa MozoCartas = new JuegoMesa("Mozo de Cartas", 2019, Nuevo, 7, 4);
+    JuegoMesa Dados = JuegoMesa("Dados", 2020, estadoDados, 2, 6);
     //JuegoMesa Dados = new JuegoMesa("Dados", 2020, Roto, 2, 6);
     return   0; 
 }
