9dbae05405dafc01235e386e9663c7e77a24bfd7
ivan1arriola <ivan.arriola@fing.edu.uy>
2023-03-19 12:31:20 -0300

    Libros declarados

--- main.cc
+++ main.cc
@@ -1,23 +1,30 @@
+//librerias
 #include <iostream>
-using namespace std;
-#include "JuegoMesa.h"
-#include "Objeto.h"
-#include "Objeto.cc"
-#include "JuegoMesa.cc"
 #include <string>
+using namespace std;
+
+//includes
+#include "JuegoMesa.h"
+#include "Libro.h"
+#include "Niño.h"
+
   
 int main()
 {
     Estado estadoUno = Estado::Roto;
     Estado estadoMozoCartas = Estado::Nuevo;
     Estado estadoDados = Estado::Roto;
-    //Creación de objetos JuegoMesa
+
+    //Juego de Mesa
     JuegoMesa Uno = JuegoMesa("Juego Uno", 2022, estadoUno, 7, 10);
-    //JuegoMesa Uno = new JuegoMesa("Juego Uno", 2022, Roto, 7, 10);
     JuegoMesa MozoCartas = JuegoMesa("Mozo de Cartas", 2019, estadoMozoCartas, 7, 4);
-    //JuegoMesa MozoCartas = new JuegoMesa("Mozo de Cartas", 2019, Nuevo, 7, 4);
     JuegoMesa Dados = JuegoMesa("Dados", 2020, estadoDados, 2, 6);
-    //JuegoMesa Dados = new JuegoMesa("Dados", 2020, Roto, 2, 6);
+
+    // Libros
+    Libro nacidosDeLaBruma = new Libro("Nacidos de la bruma: EL imperio final", 2022, new Estado::Roto, "Brandon Sanderson", 688);
+    Libro lasMalas = new Libro("Las Malas", 2022, new Estado::Nuevo, "Camila Sosa Villada", 240);
+    Libro elCocodrilo = new Libro("El cocodrilo al que no le gustaba el agua", 2016, new Estado::Roto, "Gemma Merino", 32);
+
 
     // prueba toString()
     String mostrar = Uno.toString();
