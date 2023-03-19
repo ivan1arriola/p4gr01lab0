95740018ccfccd4fa3775b178fc8bd0fa3f3b062
Rodrigo Gabriel Fernandez Rodriguez <rodrigo.gabriel.fernandez@fing.edu.uy>
2023-03-19 14:56:55 -0300

    Update src/Objeto/Niño.cc

--- src/Objeto/Niño.cc
+++ src/Objeto/Niño.cc
@@ -3,7 +3,9 @@
 #include "Niño.h"
 #include "Libro.h"
 #include "JuegoMesa.h"
-
+#include <iostream>
+#include <vector>
+#include "Objeto.h"
 using namespace std;
 
 Ninio::Ninio(){
@@ -64,3 +66,53 @@ string Ninio::getTelefono(){
 Ninio::~Ninio(){
   
 }
+
+using namespace std;
+
+class Niño {
+private:
+    string nombre;
+    int edad;
+    vector<Objeto*> objetosPrestados;
+
+public:
+    Niño(string nombre, int edad) {
+        this->nombre = nombre;
+        this->edad = edad;
+    }
+
+    string getNombre() {
+        return nombre;
+    }
+
+    int getEdad() {
+        return edad;
+    }
+
+    void prestarObjeto(Objeto* objeto) {
+        objetosPrestados.push_back(objeto);
+    }
+
+    void devolverObjeto(Objeto* objeto) {
+        for (int i = 0; i < objetosPrestados.size(); i++) {
+            if (objetosPrestados[i] == objeto) {
+                objetosPrestados.erase(objetosPrestados.begin() + i);
+                break;
+            }
+        }
+    }
+
+    void listarObjetosPrestados() {
+        cout << "Objetos prestados por " << nombre << ":" << endl;
+        for (int i = 0; i < objetosPrestados.size(); i++) {
+            cout << "- " << objetosPrestados[i]->toString() << endl;
+        }
+    }
+};
+Esta implementación asume que la clase Objeto ya está definida y tiene un método toString() implementado. También asume que los objetos se prestan y devuelven utilizando punteros a objetos Objeto.
+
+
+
+
+
+
